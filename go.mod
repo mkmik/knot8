768bc38e57ebe59282caff3a282f8c23b137541b
Marko Mikulicic <mkmik@vmware.com>
2020-04-03 18:29:42 +0200

    Parse YAMLs

--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.14
 require (
 	github.com/alecthomas/kong v0.2.4
 	github.com/mkmik/multierror v0.3.0
+	gopkg.in/yaml.v3 v3.0.0-20200313102051-9f266ea9e77c
 )
--- main.go
+++ main.go
@@ -2,9 +2,13 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
+	"os"
 
 	"github.com/alecthomas/kong"
+	"github.com/mkmik/multierror"
+	"gopkg.in/yaml.v3"
 )
 
 type Context struct {
@@ -20,24 +24,69 @@ type SetCmd struct {
 }
 
 func (s *SetCmd) Run(ctx *Context) error {
-	fmt.Printf("setting files %q, knobs: %q\n", s.Paths, s.Values)
 	files, err := openFileArgs(s.Paths)
 	if err != nil {
 		return err
 	}
-	fmt.Printf("got %v\n", files)
 	if len(files) == 0 {
 		return fmt.Errorf("cannot find any manifest in %q", s.Paths)
 	}
 
 	defer func() {
 		for _, f := range files {
-			log.Printf("closing %q\n", f.Name())
 			f.Close()
 		}
 	}()
 
+	var (
+		manifests []Manifest
+		errs      []error
+	)
+	for _, f := range files {
+		if ms, err := parseManifests(f); err != nil {
+			errs = append(errs, err)
+		} else {
+			manifests = append(manifests, ms...)
+		}
+	}
+	if errs != nil {
+		return multierror.Join(errs)
+	}
+
+	for _, m := range manifests {
+		log.Printf("--> manifest in %q, contents: %q...\n", m.file, fmt.Sprint(m.raw)[:40])
+	}
 	return nil
+}
+
+type Manifest struct {
+	APIVersion string         `json:"apiVersion"`
+	Kind       string         `json:"kind"`
+	Metadata   ObjectMetadata `json:"metadata"`
+
+	file string
+	raw  interface{}
+}
+
+type ObjectMetadata struct {
+	Annotations map[string]string `json:"annotations"`
+}
+
+func parseManifests(f *os.File) ([]Manifest, error) {
+	var res []Manifest
+	d := yaml.NewDecoder(f)
+	for {
+		var i interface{}
+		if err := d.Decode(&i); err == io.EOF {
+			break
+		} else if err != nil {
+			return nil, err
+		}
+
+		res = append(res, Manifest{file: f.Name(), raw: i})
+
+	}
+	return res, nil
 }
 
 func main() {

335a5a0af6298a75c1a274d4554c4a87a4505bf7
Marko Mikulicic <mkmik@vmware.com>
2020-04-06 19:01:06 +0200

    Switch to table testing

--- pkg/kptr/kptr_test.go
+++ pkg/kptr/kptr_test.go
@@ -2,6 +2,7 @@ package kptr_test
 
 import (
 	"errors"
+	"fmt"
 	"testing"
 
 	kptr "github.com/mkmik/kno8/pkg/kptr"
@@ -26,38 +27,33 @@ spec:
 		t.Fatalf("expecting not found error, got: %v", err)
 	}
 
-	r, err := kptr.Find(&root, `/spec/template/spec/replicas`)
-	if err != nil {
-		t.Fatal(err)
-	}
-	if got, want := r.Line, 5; got != want {
-		t.Fatalf("got: %v, want: %v", got, want)
-	}
-	if got, want := r.Column, 17; got != want {
-		t.Fatalf("got: %v, want: %v", got, want)
-	}
-
-	r, err = kptr.Find(&root, `/spec/template/spec/containers/0/image`)
-	if err != nil {
-		t.Fatal(err)
-	}
-	if got, want := r.Value, "nginx"; got != want {
-		t.Fatalf("got: %v, want: %v", got, want)
+	testCases := []struct {
+		ptr    string
+		value  string
+		line   int
+		column int
+	}{
+		{`/spec/template/spec/replicas`, "1", 5, 17},
+		{`/spec/template/spec/containers/0/image`, "nginx", 8, 16},
+		{`/spec/template/spec/containers/~{"name":"app"}/image`, "nginx", 8, 16},
+		{`/spec/template/spec/containers/~[name=app]/image`, "nginx", 8, 16},
 	}
 
-	r, err = kptr.Find(&root, `/spec/template/spec/containers/~{"name":"app"}/image`)
-	if err != nil {
-		t.Fatal(err)
-	}
-	if got, want := r.Value, "nginx"; got != want {
-		t.Fatalf("got: %v, want: %v", got, want)
-	}
-
-	r, err = kptr.Find(&root, `/spec/template/spec/containers/~[name=app]/image`)
-	if err != nil {
-		t.Fatal(err)
-	}
-	if got, want := r.Value, "nginx"; got != want {
-		t.Fatalf("got: %v, want: %v", got, want)
+	for i, tc := range testCases {
+		t.Run(fmt.Sprint(i), func(t *testing.T) {
+			r, err := kptr.Find(&root, tc.ptr)
+			if err != nil {
+				t.Fatal(err)
+			}
+			if got, want := r.Value, tc.value; got != want {
+				t.Fatalf("got: %v, want: %v", got, want)
+			}
+			if got, want := r.Line, tc.line; got != want {
+				t.Errorf("got: %v, want: %v", got, want)
+			}
+			if got, want := r.Column, tc.column; got != want {
+				t.Errorf("got: %v, want: %v", got, want)
+			}
+		})
 	}
 }

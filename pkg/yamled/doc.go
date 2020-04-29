564caebfc572e4141e3968f40a09fcec154842eb
Marko Mikulicic <mkmik@vmware.com>
2020-04-30 01:46:42 +0200

    Add yamled pkg skel

---
+++ pkg/yamled/doc.go
@@ -0,0 +1,9 @@
+// Copyright 2020 VMware, Inc.
+// SPDX-License-Identifier: BSD-2-Clause
+
+/*
+Package yamled implements in-place editing of YAML sources.
+*/
+package yamled
+
+// TODO: move bits from cmd/knot/*.go

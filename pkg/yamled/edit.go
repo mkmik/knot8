2c9cbf5eacac315a426c45da069d1a69d8ff2888
Marko Mikulicic <mkmik@vmware.com>
2020-04-30 03:02:15 +0200

    Remove unnecessary boundary checks, already covered by runtime

--- pkg/yamled/edit.go
+++ pkg/yamled/edit.go
@@ -5,7 +5,6 @@ package yamled
 
 import (
 	"bytes"
-	"fmt"
 	"sort"
 
 	"gopkg.in/yaml.v3"
@@ -63,17 +62,7 @@ func Splice(buf RuneSplicer, edits []Edit) error {
 // A RuneBuffer is a trivial implementation of a RuneSplicer that uses a rune slice.
 type RuneBuffer []rune
 
-func (buf RuneBuffer) boundsCheck(start, end int) error {
-	if l := len(buf); start < 0 || start >= l || end < start || end >= l {
-		return fmt.Errorf("%d:%d out of bound (buf size %d)", start, end, l)
-	}
-	return nil
-}
-
 func (buf *RuneBuffer) Splice(value string, start, end int) error {
-	if err := buf.boundsCheck(start, end); err != nil {
-		return err
-	}
 	*buf = append((*buf)[:start], append(bytes.Runes([]byte(value)), (*buf)[end:]...)...)
 	return nil
 }

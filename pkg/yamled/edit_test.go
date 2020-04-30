0fc8dcf7bb86fda933b3bcf3cf448fab6ce5dfca
Marko Mikulicic <mkmik@vmware.com>
2020-04-30 03:26:44 +0200

    Table test

--- pkg/yamled/edit_test.go
+++ pkg/yamled/edit_test.go
@@ -1,6 +1,7 @@
 package yamled_test
 
 import (
+	"fmt"
 	"testing"
 
 	"gopkg.in/yaml.v3"
@@ -9,41 +10,111 @@ import (
 )
 
 func TestSplice(t *testing.T) {
-	src := `foo: abc
+	src1 := `foo: abc
 bar: xy
 baz: end
 `
 
-	buf := yamled.RuneBuffer(src)
-	var n yaml.Node
-	if err := yaml.Unmarshal([]byte(string(buf)), &n); err != nil {
-		t.Fatal(err)
+	testCases := []struct {
+		src  string
+		foo  string
+		bar  string
+		want string
+	}{
+		{
+			src: src1,
+			foo: "AB",
+			bar: "xyz",
+		},
+		{
+			src: src1,
+			foo: "ABCD",
+			bar: "x",
+		},
+		{
+			src: src1,
+			foo: "ABCD",
+			bar: "",
+		},
+		{
+			src: src1,
+			foo: "",
+			bar: "x",
+		},
+		{
+			src: src1,
+			foo: "",
+			bar: "a#b",
+		},
+		/* TODO
+		{
+			src: src1,
+			foo: "",
+			bar: "a #b",
+		},
+		{
+			src: src1,
+			foo: "",
+			bar: " ",
+		},
+		{
+			src: src1,
+			foo: "",
+			bar: "\n",
+		},
+		{
+			src: src1,
+			foo: "1",
+			bar: "2",
+		},
+		*/
 	}
 
-	foo, err := yptr.Find(&n, "/foo")
-	if err != nil {
-		t.Fatal(err)
-	}
+	for i, tc := range testCases {
+		t.Run(fmt.Sprint(i), func(t *testing.T) {
+			buf := yamled.RuneBuffer(tc.src)
+			var n yaml.Node
+			if err := yaml.Unmarshal([]byte(string(buf)), &n); err != nil {
+				t.Fatal(err)
+			}
 
-	bar, err := yptr.Find(&n, "/bar")
-	if err != nil {
-		t.Fatal(err)
-	}
+			foo, err := yptr.Find(&n, "/foo")
+			if err != nil {
+				t.Fatal(err)
+			}
 
-	edits := []yamled.Edit{
-		yamled.NewEdit("AB", foo),
-		yamled.NewEdit("xyz", bar),
-	}
-	if err := yamled.Splice(&buf, edits); err != nil {
-		t.Fatal(err)
-	}
+			bar, err := yptr.Find(&n, "/bar")
+			if err != nil {
+				t.Fatal(err)
+			}
 
-	want := `foo: AB
-bar: xyz
-baz: end
-`
+			edits := []yamled.Edit{
+				yamled.NewEdit(tc.foo, foo),
+				yamled.NewEdit(tc.bar, bar),
+			}
+			if err := yamled.Splice(&buf, edits); err != nil {
+				t.Fatal(err)
+			}
 
-	if got := string(buf); got != want {
-		t.Errorf("got: %q, want: %q", got, want)
+			var ne yaml.Node
+			if err := yaml.Unmarshal([]byte(string(buf)), &ne); err != nil {
+				t.Fatal(err)
+			}
+
+			check := func(path, want string) {
+				f, err := yptr.Find(&ne, path)
+				if err != nil {
+					t.Fatal(err)
+				}
+				if got := f.Value; got != want {
+					t.Errorf("got: %q, want: %q", got, want)
+				}
+				if tag := f.Tag; tag != "!!str" && tag != "!!null" {
+					t.Errorf("tag for %q must be either string or null, got %q", path, tag)
+				}
+			}
+			check("/foo", tc.foo)
+			check("/bar", tc.bar)
+		})
 	}
 }

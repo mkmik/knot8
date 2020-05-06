10b8ce40466b3af81fdc5e3d9577cf6b4fe9cdc4
Marko Mikulicic <mkmik@vmware.com>
2020-05-06 14:41:36 +0200

    Factor out common scanning/transform code

--- pkg/yptr/yamled/edit.go
+++ pkg/yptr/yamled/edit.go
@@ -23,6 +23,10 @@ type Replacement struct {
 	value string
 }
 
+func (r Replacement) asReplacer() replacer {
+	return replacer{r.ext, func(prev string) (string, error) { return quote(r.value, prev) }}
+}
+
 // NewReplacement constructs a new Replacement structure from a value and a yaml.Node.
 func NewReplacement(value string, node *yaml.Node) Replacement {
 	return Replacement{NewExtent(node), value}
@@ -30,7 +34,45 @@ func NewReplacement(value string, node *yaml.Node) Replacement {
 
 // Replace copies text from r to w while replacing text at given rune extents,
 // as specified by the reps slice.
-func Replace(w io.Writer, r io.Reader, reps []Replacement) error {
+func Replace(w io.Writer, r io.Reader, rs []Replacement) error {
+	reps := make([]replacer, len(rs))
+	for i := range rs {
+		reps[i] = rs[i].asReplacer()
+	}
+	return transform(w, r, reps)
+}
+
+// Extract returns a slice of strings for each extent of the input reader.
+// The order of the resulting slice matches the order of the provided exts slice
+// (which can be in any order; extract provides the necessary sorting to guarantee a single
+// scan pass on the reader).
+func Extract(r io.Reader, exts []Extent) ([]string, error) {
+	var (
+		reps = make([]replacer, len(exts))
+		res  = make([]string, len(exts))
+	)
+	for i, ext := range exts {
+		i := i
+		reps[i] = replacer{ext, func(prev string) (string, error) {
+			res[i] = prev
+			return prev, nil
+		}}
+	}
+	if err := transform(ioutil.Discard, r, reps); err != nil {
+		return nil, err
+	}
+	return res, nil
+}
+
+type replacer struct {
+	ext     Extent
+	replace func(prev string) (string, error)
+}
+
+// transform copies text from r to w while replacing text at given rune extents,
+// as specified by the reps slice. The text to be replaced is provided via a callback
+// function "replace" in the replacer structures.
+func transform(w io.Writer, r io.Reader, reps []replacer) error {
 	wbuf, rbuf := bufio.NewWriter(w), bufio.NewReader(r)
 	defer wbuf.Flush()
 
@@ -38,8 +80,8 @@ func Replace(w io.Writer, r io.Reader, reps []Replacement) error {
 
 	pos := 0
 	var prev bytes.Buffer
-	for _, ri := range rmap {
-		rep := reps[ri]
+	for _, i := range rmap {
+		rep := reps[i]
 
 		// Copy out the span until the start of the current extent.
 		if err := copyRunesN(wbuf, rbuf, rep.ext.Start-pos); err != nil {
@@ -52,16 +94,14 @@ func Replace(w io.Writer, r io.Reader, reps []Replacement) error {
 			return err
 		}
 
-		q, err := quote(rep.value, prev.String())
+		next, err := rep.replace(prev.String())
 		if err != nil {
 			return err
 		}
-		prev.Reset()
-
-		// Emit the quoted string
-		if _, err := wbuf.WriteString(q); err != nil {
+		if _, err := wbuf.WriteString(next); err != nil {
 			return err
 		}
+		prev.Reset()
 
 		pos = rep.ext.End
 	}
@@ -69,45 +109,7 @@ func Replace(w io.Writer, r io.Reader, reps []Replacement) error {
 	// Copy out the trailing span.
 	_, err := io.Copy(wbuf, rbuf)
 	return err
-}
 
-// Extract returns a slice of strings for each extent of the input reader.
-// The order of the resulting slice matches the order of the provided exts slice
-// (which can be in any order; extract provides the necessary sorting to guarantee a single
-// scan pass on the reader).
-func Extract(r io.Reader, exts []Extent) ([]string, error) {
-	res := make([]string, len(exts))
-
-	wbuf, rbuf := bufio.NewWriter(ioutil.Discard), bufio.NewReader(r)
-
-	emap := argsort.SortSlice(exts, func(i, j int) bool { return exts[i].Start < exts[j].Start })
-
-	pos := 0
-	var prev bytes.Buffer
-	for _, i := range emap {
-		ext := exts[i]
-		// Copy out the span until the start of the current extent.
-		if err := copyRunesN(wbuf, rbuf, ext.Start-pos); err != nil {
-			return nil, err
-		}
-
-		// Consume the old content of the extent to be replaced.
-		// Save it into a buffer because the quoting heuristic needs the previous value.
-		if err := copyRunesN(&prev, rbuf, ext.End-ext.Start); err != nil {
-			return nil, err
-		}
-		res[i] = prev.String()
-		prev.Reset()
-
-		pos = ext.End
-	}
-
-	// Copy out the trailing span.
-	_, err := io.Copy(wbuf, rbuf)
-	if err != nil {
-		return nil, err
-	}
-	return res, nil
 }
 
 type runeWriter interface {
